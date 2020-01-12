3aae97e4b52df01d0cfde272d3c637b520fa14ea
Nathan <nathan.marson@my.jcu.edu.au>
2020-01-12 13:35:56 +1000

    update database settings

--- controlapi/settings.py
+++ controlapi/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'controls',
-        'USER': 'test',
-        'PASSWORD': 'test',
+        'USER': 'user',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': ''
     }
